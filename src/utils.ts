359a830c077b9b7ac7bcc8a9a1b7d2ccd56c8c84
irenlian <irynalian@stockx.com>
2022-01-03 22:38:31 +0200

    Debugging

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
 import { SUB_DIRECTORY } from '~/config';
 
-export const path = (p: string) => SUB_DIRECTORY + p;
+export const path = (p: string) => {
+  console.log(SUB_DIRECTORY);
+  return SUB_DIRECTORY + p;
+};
